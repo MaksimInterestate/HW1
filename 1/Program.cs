794e0bf9d0eb9df68dd71fb7ad75e4b464147849
Max <zenit-max@example.com>
2022-09-20 17:42:21 +0300

    Задача 1

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+    
+
+int a = 2;
+int b = 10;
+if(a > b)
+{
+    Console.WriteLine("max = " + a);
+} 
+else if(a < b)
+{
+    Console.WriteLine("max = " + b);
+}
+
