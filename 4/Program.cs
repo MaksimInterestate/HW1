c224b041c5faf247f18cefe46a5014872b306d00
Max <zenit-max@example.com>
2022-09-21 13:46:45 +0300

    Задача 4

---
+++ 4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Ваше число " + n);
+int chetnoe = 1;
+int max = n;
+while (chetnoe <= max)
+{
+    if (chetnoe % 2 == 0) Console.Write(chetnoe + ", ");
+    chetnoe ++;
+}
