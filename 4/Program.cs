4f85df24a8813f8618c983ddd3ce54b3a493ba5b
Max <zenit-max@example.com>
2022-09-24 17:27:46 +0300

    Задача 004

--- 4/Program.cs
+++ 4/Program.cs
@@ -6,8 +6,15 @@ int n = int.Parse(Console.ReadLine());
 Console.WriteLine("Ваше число " + n);
 int chetnoe = 1;
 int max = n;
+Console.WriteLine("Все четные числа от 1 до " + n );
 while (chetnoe <= max)
 {
-    if (chetnoe % 2 == 0) Console.Write(chetnoe + ", ");
+    if (chetnoe % 2 == 0) 
+    {
+        Console.Write(chetnoe + ", ");
+    }
     chetnoe ++;
 }
+
+
+
