e5ea78e82966f4ac0019e5e9775c4fb4d57849d1
Max <zenit-max@example.com>
2022-09-24 17:27:54 +0300

    Задача 005

---
+++ 5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+int m =1;
+int f = 1;
+
+while(m <= N)
+{   
+    f = f * m;
+    m ++;
+}
+Console.WriteLine("Факториал числа " + N +" = " + f);
